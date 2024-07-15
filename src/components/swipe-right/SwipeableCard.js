a8dfc4c8f234773688d9a22281a58b974b57974c
akankshar-oy <146951245+akankshar-oy@users.noreply.github.com>
2024-07-15 20:34:57 +0530

    Update SwipeableCard.js

--- src/components/swipe-right/SwipeableCard.js
+++ src/components/swipe-right/SwipeableCard.js
@@ -2,27 +2,29 @@ import React, { useState, useEffect } from 'react';
 import { useSwipeable } from 'react-swipeable';
 import './swipe-card.css';
 
-const SwipeableCard = ({ character, onSwiped, index, currentIndex }) => {
+const SwipeableCard = ({ character, index, currentIndex, onSwiped }) => {
     const [swipeDirection, setSwipeDirection] = useState('');
 
+    // Swipeable handlers for left and right swipes
     const handlers = useSwipeable({
-        onSwipedLeft: () => {
-            setSwipeDirection('left');
-            onSwiped('left', character.name);
-        },
-        onSwipedRight: () => {
-            setSwipeDirection('right');
-            onSwiped('right', character.name);
-        },
+        onSwipedLeft: () => handleSwipe('left'),
+        onSwipedRight: () => handleSwipe('right'),
         preventDefaultTouchmoveEvent: true,
         trackMouse: true,
     });
 
+    // Function to handle swipe direction and trigger onSwiped callback
+    const handleSwipe = (direction) => {
+        setSwipeDirection(direction); // Set swipe direction for CSS animation
+        onSwiped(direction, character.name); // Trigger parent component callback
+    };
+
+    // Reset swipe direction when currentIndex changes
     useEffect(() => {
-        if (index === currentIndex) {
+        if (index !== currentIndex) {
             setSwipeDirection('');
         }
-    }, [currentIndex, index]);
+    }, [index, currentIndex]);
 
     return (
         <div className={`swipeable-card ${swipeDirection}`} style={{ zIndex: index === currentIndex ? 1 : 0 }}>
